66b2019ea3529fb320f1c11f06ad903fa1e44263
PrincessMaggy <edoziemagdalene@gmail.com>
2021-09-02 21:12:13 -0700

    added some features for the calculator

--- index.js
+++ index.js
@@ -22,3 +22,47 @@
            }
        }, false );
  }( document ));
+
+
+//  CALCULATOR
+const keys = document.querySelector('.calculator__keys');
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    if (!action) {
+        console.log('number key!')
+      }
+      else if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        console.log('operator key!')
+      }
+      else if (action === 'decimal') {
+        console.log('decimal key!')
+      }
+      
+      else if (action === 'clear') {
+        console.log('clear key!')
+      }
+      
+      else if (action === 'calculate') {
+        console.log('equal key!')
+      }
+ }
+})
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    // ...
+  }
+})
